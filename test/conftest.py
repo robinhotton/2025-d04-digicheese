5a5026e5d5f4f778011cb646db72f20842087b18
Robin HOTTON <robin.hotton@hotmail.com>
2025-07-28 11:28:15 +0200

    refacto

--- test/conftest.py
+++ test/conftest.py
@@ -1,27 +1,52 @@
-from fastapi.testclient import TestClient
+##################
+# Modules import #
+##################
+
 import pytest
-from src.database import get_session
-from src.main import app
+from fastapi.testclient import TestClient
 from sqlmodel import create_engine, Session, SQLModel
 
-db_url = "sqlite:///./test.db"
-engine = create_engine(db_url, echo=False)
+###############
+# SRC imports #
+###############
 
-@pytest.fixture(scope="function", autouse=True)
-def setup_database():
+from src.main import app
+from src.database import get_session
+from src.models.client import Client as ClientModel
+
+############
+# Fixtures #
+############
+
+@pytest.fixture(scope="session")
+def test_session():
+    """
+    Crée une base de données SQLite en fichier pour vérifier.
+    Initialise les tables, et insère un permet d'insérer des données par défaut.
+    """
+    db_url = "sqlite:///./test.db"
+    engine = create_engine(db_url, echo=False)
     SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
-    yield
-
-def override_get_session():
-    testing_session = Session(engine, autocommit=False, autoflush=False)
-    try:
-        yield testing_session
-    finally:
-        testing_session.close()
+    
+    with Session(engine) as session:
+        john_doe = ClientModel(firstname="John", lastname="Doe", address_line_1="123 Cheese St")
+        session.add(john_doe)
+        session.commit()
         
-app.dependency_overrides[get_session] = override_get_session
+        # Retourne la session de test
+        yield session
 
-@pytest.fixture(scope="module")
-def client():
-    return TestClient(app)
+@pytest.fixture(scope="function")
+def client(test_session):
+    """Crée un client FastAPI qui utilise la session de test en override."""
+    def override_get_session():
+        yield test_session
+        
+    # Ecrase la connexion à l'ancienne base de données par la nouvelle
+    app.dependency_overrides[get_session] = override_get_session
+
+    with TestClient(app) as test_client:
+        yield test_client
+
+    app.dependency_overrides.clear()
