326365c4a33586e1b8e5d85955ad01decac9fe55
Robin HOTTON <robin.hotton@hotmail.com>
2025-07-28 12:51:08 +0200

    test data

--- test/test_client.py
+++ test/test_client.py
@@ -4,8 +4,11 @@ from fastapi.testclient import TestClient
 BASE_URL = "/api/v1"
 
 def test_get_all_clients(client: TestClient):
-    result = client.get(f"{BASE_URL}/clients")
+    result: Response = client.get(f"{BASE_URL}/clients")
     assert result.status_code == 200
+    assert isinstance(result.json(), list)
+    data = result.json()
+    assert len(data) > 0
     
 def test_get_client_by_id(client: TestClient):
     pass
